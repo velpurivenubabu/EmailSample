94a61d38e9903542265e3bae789dc7b82f9a2bee
venubabu <velpurivenubabu123@gmail.com>
2023-05-21 13:00:46 +0530

    [Venubabu] Done With Sample Test EmailsUC1

---
+++ EmailSample/Email Samples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EmailSample
+{
+    public class MyPattern
+    {
+
+        public  string email = "^[a-z]{3,}(([.|+]{1})?([-]{1})?[0-9]{1,})?@[a-z0-9]{1,}.[a-z]{3}(.[a-z]{2,4})?$";
+
+
+        public string ValidateEmailName(string data)
+        {
+            string result = Regex.IsMatch(data, email) ? "Valid" : "Invalid";
+            return result;
+
+        }
+    }
+}
--- EmailSample/Program.cs
+++ EmailSample/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace EmailSample
+﻿using System;
+
+namespace EmailSample
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
+        {MyPattern myObj = new MyPattern();
             Console.WriteLine("Hello, World! Welcome to Email Sample Programme");
+            Console.WriteLine("Valid Email Sampiles");
+            Console.WriteLine(myObj.ValidateEmailName("abc@yaho.com"));
+            Console.WriteLine(myObj.ValidateEmailName("abc-100@yaho.com"));
+            Console.WriteLine(myObj.ValidateEmailName("abc.100@yaho.com"));
+            Console.WriteLine(myObj.ValidateEmailName("abc.111@yaho.com"));
+            Console.WriteLine(myObj.ValidateEmailName("abc.100@yaho.net"));
+            Console.WriteLine(myObj.ValidateEmailName("abc.111@yaho.com.au"));
+            Console.WriteLine(myObj.ValidateEmailName("abc@gmail.com.com"));
+            Console.WriteLine(myObj.ValidateEmailName("abc+100@gmail.com"));
+            Console.WriteLine("InValid Email Sampiles");
+            Console.WriteLine(myObj.ValidateEmailName("abc"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc@.com.my"));
+            Console.WriteLine(myObj.ValidateEmailName("abc123@gmail.a"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc123@gmail.com.com"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc123@.abc.com"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc()*@gmail.com"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc@@gmail.com.aa.au"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc@abc@gmail.com"));
+            Console.WriteLine(myObj.ValidateEmailName(".abc..@gmail.com"));
+
+
         }
     }
 }
